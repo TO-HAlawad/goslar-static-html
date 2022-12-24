50bbc11341780046993e876e4b22501457c1a92b
Hekmat Alawad <hekmat.alawad@dirs21.team>
2022-12-24 22:06:44 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../views/home.vue";
-import AusstattungPreise from "../views/ausstattung-preise.vue";
-import OnlineBuchen from "../views/online-buchen.vue";
-import Goslar from "../views/goslar.vue";
-import Anreise from "../views/anreise.vue";
-import Kontakt from "../views/kontakt.vue";
-import Impressum from "../views/impressum.vue";
+import HomePage from "./views/home.vue";
+import AusstattungPreise from "./views/ausstattung-preise.vue";
+import OnlineBuchen from "./views/online-buchen.vue";
+import Goslar from "./views/goslar.vue";
+import Anreise from "./views/anreise.vue";
+import Kontakt from "./views/kontakt.vue";
+import Impressum from "./views/impressum.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
